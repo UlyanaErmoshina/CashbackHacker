d2afd85b657cca499037eb5bd67b7d5d3883b265
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-07-28 12:06:53 +0300

    test commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,25 +10,24 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int actual = bonusService.remain(amount);
         int expected = 100;
-        assertEquals (expected,actual);
-
+        assertEquals(expected,actual);
     }
+
     @Test
     public void shouldCalculateHowMuchBuy2() {
         CashbackHackService bonusService= new CashbackHackService();
         int amount = 0;
         int actual = bonusService.remain(amount);
         int expected = 1000;
-        assertEquals (expected,actual);
-
+        assertEquals(expected,actual);
     }
+
     @Test
     public void shouldCalculateHowMuchBuy3() {
         CashbackHackService bonusService= new CashbackHackService();
         int amount = 1000;
         int actual = bonusService.remain(amount);
         int expected = 0;
-        assertEquals (expected,actual);
-
+        assertEquals(expected,actual);
     }
 }
